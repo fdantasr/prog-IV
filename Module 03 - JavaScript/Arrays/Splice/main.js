be905fcc66bf95259d63f6bd8d505675c9d3511e
Felipe Dantas <dantas.contatod@gmail.com>
2023-05-03 15:41:19 -0300

    arrays basic

---
+++ Module 03 - JavaScript/Arrays/Splice/main.js
@@ -0,0 +1,5 @@
+const names = ['Felipe', 'Lucas', 'Djalma', 'Joao', 'Wesley'];
+console.log(names);
+const removidos = names.splice(3, 2); // Remove o elemento do array, a partir do índice 4, e remove 1 elemento
+
+console.log(names, removidos);
